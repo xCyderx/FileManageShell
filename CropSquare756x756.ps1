310577623b9421f7126bc78880083108501e08d0
Shinya Nishida <Shinya.Michael.Nishida@gmail.com>
2024-11-11 20:00:45 +0900

    Add files via upload

---
+++ CropSquare756x756.ps1
@@ -0,0 +1,26 @@
+﻿<#
+Short Description:
+    指定ディレクトリ内のすべてのJPG画像を、指定された範囲にトリミングして保存するPowerShellスクリプト
+
+Long Description:
+    このPowerShellスクリプトは、現在のディレクトリ内に存在するすべてのJPGファイルを検索し、
+    それぞれに対して特定の範囲でトリミング処理を実行します。
+    スクリプトはまず、.jpg拡張子を持つファイル名のリストを取得し、
+    リスト内の各ファイルに対してImageMagickのconvertコマンドを使用して、
+    922x801ピクセルのサイズで切り取ります。
+    切り取りは、画像の左から215ピクセル、上から66ピクセルを開始点とします。
+    トリミングされた画像は、元のファイル名に「Final」を追加した新しいファイル名で保存されます。
+#>
+
+# JPEGファイル（.jpg）をフルパスではなく名前のみで取得し、$file_listに格納
+$file_list = ls -Name *.jpg
+
+# ループで$file_listの各ファイルに処理を行う
+for($i=0; $i -lt $file_list.count; $i++) {
+    # 新しいファイル名を設定（Outputというフォルダ 元のファイル名）
+    $file_new_name = "Output\\" + $file_list[$i]
+    
+    # ImageMagickを使用して画像を指定された範囲にトリミングし、新しい名前で保存
+    # -cropオプション: 768x768サイズにトリミングし、左から256px、上から128pxを開始点とする
+    magick convert $file_list[$i] -crop 768x768+256+128 "NewFile\\"$file_new_name
+}
---
+++ ShinyaMove.ps1
@@ -0,0 +1,17 @@
+﻿<#
+Rename and Move
+#>
+
+$folder_list = Get-ChildItem -Name
+
+mkdir "NewFile"
+
+for($i=0; $i -lt $folder_list.count; $i++)
+{
+    $childfolder = Get-ChildItem $folder_list[$i] -Name
+    $childfolder = ([string]::Format("{0}\\{1}",$folder_list[$i],$childfolder))
+    $filename = Get-ChildItem $childfolder -Name
+    $filename = ([string]::Format("{0}\\{1}",$childfolder,$filename))
+
+    mv $filename ([string]::Format("{0}.jpg",$folder_list[$i]))
+}
